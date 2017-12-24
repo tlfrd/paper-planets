c59e6c797cf7da5c357274ec031b1cbe629789d4
Cale Tilford <combinekidd@gmail.com>
2017-12-24 00:11:01 +0000

    Use modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,8 @@
 import style from '../styles/main.scss';
-
+const paperplanets = require('./paperplanets');
 const bodyTemplate = require("../templates/body.handlebars");
 
 document.addEventListener("DOMContentLoaded", function() {
   document.body.innerHTML = bodyTemplate();
+  paperplanets();
 });
